3c4228afe45c4397b77d18b0375d48d50f192fff
selam <selamawitmesfin55@gmail.com>
2025-02-01 23:45:39 -0800

    Add task_3.sql to list all tables

---
+++ task_3.sql
@@ -0,0 +1,7 @@
+-- task_3.sql: Script to list all tables in the alx_book_store database
+
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- List all the tables in the database
+SHOW TABLES;
