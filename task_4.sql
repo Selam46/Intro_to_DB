2d8a0eccb37c39a389c47b05cd8c64c4e723d8b8
selam <selamawitmesfin55@gmail.com>
2025-02-01 23:55:16 -0800

    Add task_4.sql to print table description for Books

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,10 @@
--- task_4.sql: Script to print the full description of the Books table
+USE alx_book_store;
 
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+SELECT COLUMN_NAME AS 'Column Name',
+       COLUMN_TYPE AS 'Data Type',
+       IS_NULLABLE AS 'Is Nullable',
+       COLUMN_DEFAULT AS 'Default Value',
+       EXTRA AS 'Extra Info'
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+  AND TABLE_NAME = 'Books';
